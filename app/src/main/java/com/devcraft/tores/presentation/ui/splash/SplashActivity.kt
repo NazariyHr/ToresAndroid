fa95ab8921801adea898dcb905d4f6056a2705d7
NazariyHr <nazariu11@gmail.com>
2021-05-09 23:44:29 +0300

    show version in splash screen

--- app/src/main/java/com/devcraft/tores/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/devcraft/tores/presentation/ui/splash/SplashActivity.kt
@@ -1,22 +1,29 @@
 package com.devcraft.tores.presentation.ui.splash
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.devcraft.tores.BuildConfig
 import com.devcraft.tores.R
 import com.devcraft.tores.presentation.ui.auth.AuthActivity
 import com.devcraft.tores.presentation.ui.main.MainActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashActivity : AppCompatActivity() {
 
     private val vm: SplashViewModel by viewModel()
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        tvVersion.text = "version ${BuildConfig.VERSION_NAME}"
 
         Handler(Looper.getMainLooper())
             .postDelayed(
